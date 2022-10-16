c6b5ed13504a6698a76e01c1f3398bd41e6c2f30
sick2as <103950270+sick2as@users.noreply.github.com>
2022-10-16 14:11:27 -0500

    Update StudentEx.py

--- StudentEx.py
+++ StudentEx.py
@@ -1,42 +1,23 @@
-import time
+from imghdr import tests
 import random
-names = ["Jake", "Jack", "Jason"]
-classes = "Math", "Science", "SS", "Writing", "Grammar"
-student_name = names[random.randint(0, 2)]
-student_class = [random.choice(classes)]
-assignment_grades = [
-    random.randint(70, 100),
-    random.randint(0, 100),
-    random.randint(0, 100),
-    random.randint(0, 100),
-]
-test_grades = [random.randint(70, 100), random.randint(70, 100)]
-participation_grade = [
-    random.randint(0, 100),
-    random.randint(0, 100),
-    random.randint(0, 100), ]
-student = [participation_grade, test_grades, assignment_grades]
+import time
+import json
+print("Disclaimer this is a fake person with fake grades no real person had grades recorded ")
+print("also thank you for using my software By: PTSCODING")
 
+names = ["james", "john", "joe", "Jason", "Mick", "Nina"]
+classes = ['math', 'science', 'social studies', 'reading', 'grammar']
 
 def get_time():
     return time.localtime
 
-
-def get_average(a: list):
-    total_sum = sum(a)
-    total_sum = float(total_sum)
-    return total_sum / len(a)
-
-
-def power(a, power):
-    return pow(a, power)
-
+def get_average(grade):
+    return float(sum(grade)) / len(grade)
 
 def calculate_total_average(students):
-    assignment = get_average(assignment_grades)
-    test = get_average(test_grades)
+    assignment = get_average(students["assignment"])
+    test = get_average(students["test"])
     return 0.1 * assignment + 0.7 * test + 0.2
-
 
 def assign_letter_grade(score):
     if score >= 90:
@@ -45,5 +26,24 @@ def assign_letter_grade(score):
         return "B"
     elif score >= 70:
         return "C"
+    elif score >= 60:
+        return "D"
     else:
         return "F"
+
+def get_all_studinfo():
+    print(
+        f'Name: {student["name"]}  \n Class {student["tclass"]} \n Assignments: {student["assignment"]} \n Tests: {student["test"]} \n'
+    )
+    print(
+        f"{student['name']}'s averages in every type of class is ",
+        round(calculate_total_average(student)), "\n"
+    )
+    print(
+        f"{student['name']}'s letter grade is ",
+        assign_letter_grade((calculate_total_average(student))), "\n"
+    )
+    print(
+        f"Now that you know {student['name']}'s grade you can now leave. \nHave a great day! :)")
+
+
